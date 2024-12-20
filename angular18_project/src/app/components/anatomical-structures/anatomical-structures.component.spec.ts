0dc3bfecb1e0b3eb6e45862318a091d93bcb8416
Nivedithabp <bpniveditha@gmail.com>
2024-12-20 21:46:36 +0530

    Deploy Angular project to GitHub Pages

--- angular18_project/src/app/components/anatomical-structures/anatomical-structures.component.spec.ts
+++ angular18_project/src/app/components/anatomical-structures/anatomical-structures.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AnatomicalStructuresComponent } from './anatomical-structures.component';
+import { AnatomicalStructuresService } from '../../services/anatomical-structures.service';
 
 describe('AnatomicalStructuresComponent', () => {
   let component: AnatomicalStructuresComponent;
@@ -8,9 +10,9 @@ describe('AnatomicalStructuresComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AnatomicalStructuresComponent]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, AnatomicalStructuresComponent], // Use imports for standalone component
+      providers: [AnatomicalStructuresService], // Provide the required service
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AnatomicalStructuresComponent);
     component = fixture.componentInstance;
--- angular18_project/src/app/services/anatomical-structures.service.spec.ts
+++ angular18_project/src/app/services/anatomical-structures.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AnatomicalStructuresService } from './anatomical-structures.service';
 
@@ -6,7 +7,9 @@ describe('AnatomicalStructuresService', () => {
   let service: AnatomicalStructuresService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Provide HttpClient for the service
+    });
     service = TestBed.inject(AnatomicalStructuresService);
   });
 
